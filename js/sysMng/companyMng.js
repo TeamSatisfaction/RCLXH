40d9468b7d01fec08286cc554249457e65101964
cqleiliang <312577537@qq.com>
2017-07-11 16:51:17 +0800

    企业

--- js/sysMng/companyMng.js
+++ js/sysMng/companyMng.js
@@ -1,13 +1,21 @@
 /*
 /企业管理ctrl
  */
-layui.define(['layer', 'element','form','laydate'],function (exports){
+layui.define(['layer', 'element','form','laydate','upload'],function (exports){
     var $ = layui.jquery,
         layer = layui.layer;
     var loadPage = function(url){
         var parent = window.parent.document;    //主页面的DOM
         $(parent).find("#index_frame").attr("src", url);
     };
+    layui.upload({
+        url: ''
+        ,elem: ['#test1','#test2' ]//指定原始元素，默认直接查找class="layui-upload-file"
+        ,method: 'get' //上传接口的http类型
+        ,success: function(res){
+            LAY_demo_upload.src = res.url;
+        }
+    });
     /*输出内容，注意顺序*/
     var obj = {
         loadPage : loadPage
