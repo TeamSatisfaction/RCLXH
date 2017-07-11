223248efc3bb9c65173b339d96e53feb886c615e
cqleiliang <312577537@qq.com>
2017-07-11 15:10:30 +0800

    企业

--- js/sysMng/companyMng.js
+++ js/sysMng/companyMng.js
@@ -3,37 +3,11 @@
  */
 layui.define(['layer', 'element','form','laydate'],function (exports){
     var $ = layui.jquery,
-        layer = layui.layer,
-        laydate = layui.laydate;
+        layer = layui.layer;
     var loadPage = function(url){
         var parent = window.parent.document;    //主页面的DOM
         $(parent).find("#index_frame").attr("src", url);
     };
-    var start = {
-        min: laydate.now()
-        ,max: '2099-06-16 23:59:59'
-        ,istoday: false
-        ,choose: function(datas){
-            end.min = datas; //开始日选好后，重置结束日的最小日期
-            end.start = datas //将结束日的初始值设定为开始日
-        }
-    };
-    var end = {
-        min: laydate.now()
-        ,max: '2099-06-16 23:59:59'
-        ,istoday: false
-        ,choose: function(datas){
-            start.max = datas; //结束日选好后，重置开始日的最大日期
-        }
-    };
-    document.getElementById('LAY_demorange_s').onclick = function(){
-        start.elem = this;
-        laydate(start);
-    }
-    document.getElementById('LAY_demorange_e').onclick = function(){
-        end.elem = this
-        laydate(end);
-    }
     /*输出内容，注意顺序*/
     var obj = {
         loadPage : loadPage
