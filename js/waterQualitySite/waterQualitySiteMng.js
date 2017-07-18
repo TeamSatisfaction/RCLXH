e3a397220492b4f4dde3b83d591bd73cc9023967
cqleiliang <312577537@qq.com>
2017-07-18 09:20:17 +0800

    企业

---
+++ js/waterQualitySite/waterQualitySiteMng.js
@@ -0,0 +1,29 @@
+layui.define(['layer', 'element','layedit', 'laydate'],function (exports){
+    var $ = layui.jquery,
+        laydate = layui.laydate;
+        // layer = layui.layer,
+    var start = {
+        istime: true
+        ,format: 'YYYY-MM-DD hh:mm:ss'
+        ,issure: true
+    };
+    var end = {
+        istime: true
+        ,format: 'YYYY-MM-DD hh:mm:ss'
+        ,issure: true
+    };
+    document.getElementById('startTime1').onclick = function(){
+        start.elem = this;
+        laydate(start);
+    };
+    document.getElementById('endTime1').onclick = function(){
+        start.elem = this;
+        laydate(end);
+    };
+    $('#startTime1').val(laydate.now(0, "YYYY/MM/DD 00:00:00"));
+    $('#endTime1').val(laydate.now(0, "YYYY/MM/DD hh:mm:ss"));
+    var obj = {
+    };
+    /*输出内容，注意顺序*/
+    exports('waterQualitySiteMng',obj)
+})
