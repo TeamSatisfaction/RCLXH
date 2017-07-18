36903c856222a70fe138ba33e347f949029fd43e
wolegequdewow <wolegequdewow@163.com>
2017-07-18 15:01:11 +0800

    paikou

--- js/sysMng/companyMng.js
+++ js/sysMng/companyMng.js
@@ -128,24 +128,38 @@ layui.define(['layer', 'element','laypage','form','laydate','upload'],function (
             btn: ['提交', '关闭'],
             btnAlign: 'c',
             yes : function (index) {
-                layer.msg('提交成功！', {icon: 1})
+                layer.msg('提交成功！', {icon: 1});
                 layer.close(index);
+            }
+            ,zIndex: layer.zIndex //重点1
+            ,success: function(layero){
+                layer.setTop(layero); //重点2
             }
         })
     };
 
     var addPk = function () {
         layer.open({
-            title : '新增排口',
-            type : 1,
-            moveOut: true,
-            area : ['800px','560px'],
-            content : $('#pk_window'),
-            btn: ['提交', '关闭'],
-            btnAlign: 'c',
-            yes : function (index) {
-                layer.msg('提交成功！', {icon: 1})
-                layer.close(index);
+            type: 1 //此处以iframe举例
+            ,title: '当你选择该窗体时，即会在最顶端'
+            ,area: ['390px', '260px']
+            ,shade: 0
+            ,maxmin: true
+            ,offset: [ //为了演示，随机坐标
+                Math.random()*($(window).height()-300)
+                ,Math.random()*($(window).width()-390)
+            ]
+            ,content: 'pk_window'
+            ,btn: ['继续弹出', '全部关闭'] //只是为了演示
+            ,yes: function(){
+                $(that).click();
+            }
+            ,btn2: function(){
+                layer.closeAll();
+            }
+            ,zIndex: layer.zIndex //重点1
+            ,success: function(layero){
+                layer.setTop(layero); //重点2
             }
         })
     };
